cc7c86ea9f352f8246918e8c226951a59eaaa1bb
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-09-24 17:23:14 +0000

    ascii heart

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-import "bootstrap";
-import "./style.css";
+const asciiHeart = [
+  "⣠⣤⣶⣶⣦⣄⡀  ⠀⢀⣤⣴⣶⣶⣤⣀",
+  "⣼⣿⣿⣿⣿⣿⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣧",
+  "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+  "⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏",
+  " ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋ ",
+  "   ⠙⢿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀",
+  "     ⠉⢿⣿⣿⣿⠟⠋⠀",
+  "⠀      ⠙⠻⠁"
+]
+/**
+ * Imprime el corazón carácter por carácter con retardo.
+ * @param {string[]} lines - Array de strings que representan el corazón.
+ * @param {number} ms - Milisegundos de retardo entre caracteres.
+ */
+function printCharByChar(lines, ms) {
+  let totalDelay = 0
 
+  lines.forEach(line => { //
+    line.split('').forEach(char => {
+      setTimeout(() => process.stdout.write(char), totalDelay)
+      totalDelay += ms
+    })
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    // Agregar salto de línea después de cada línea completa
+    setTimeout(() => process.stdout.write("\n"), totalDelay)
+    totalDelay += ms
+  })
+}
+printCharByChar(asciiHeart, 100)
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+/*
+ * Imprime el corazón línea por línea con retardo.
+ * @param {string[]} lines - Array de strings que representan el corazón.
+ * @param {number} ms - Milisegundos de retardo entre líneas.
+ 
+function printLineByLine(lines, ms) {
+  lines.forEach((line, index) => {
+    setTimeout(() => console.log(line), index * ms)
+  })
+}
+
+// Ejecución
+
+// printLineByLine(asciiHeart, 300)*/
